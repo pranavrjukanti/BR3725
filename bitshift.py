5134b7ef45a26da948bcc43316b0093a07d845a0
pranavrjukanti <157426842+pranavrjukanti@users.noreply.github.com>
2025-03-07 13:48:25 -0800

    Update bitshift.py
    Pranav's submission of the bell ringer 3/7/25

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,18 @@
-def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
+#Bell Ringer 3/7/25
 
+#When you left-shift a binary number, multiply it by 2. Each shift to the left doubles the number. 
+# To multiply two numbers using bit shifts, we look at the bits of the second number and add the 
+# first number shifted by the right amount. For example, 
+# to multiply 5 by 3, we shift 5 and add the results when the bits of 3 are 1.
+#Attempt was not a success
+
+def multiply(p, j):
+    pranav = 0
+    shift_count = 0
+    while j > 0:
+        if j & 1:  
+            pranav += a << shift_count  
+        p <<= 1   #Examples of double shifts
+        j >>= 1  
+        shift_count += 1
+    return pranav
